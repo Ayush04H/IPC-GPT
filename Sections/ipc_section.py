31e97d0d46a009380ebca369bf8cfca482044b70
Ayush04H <ayush050419@gmail.com>
2025-01-07 16:01:18 +0530

    68-116 Ipc Sections Scraped

--- Sections/ipc_section.py
+++ Sections/ipc_section.py
@@ -47,4 +47,4 @@ def generate_and_save_ipc_sections(start, end, save_dir=None):
         print(f"Saved content for IPC Section {section_number} to {file_name}")
 
 # Call the function with a desired range and directory
-generate_and_save_ipc_sections(67,77, save_dir="Sections")  # Change range and directory as needed
+generate_and_save_ipc_sections(111,116, save_dir="Sections")  # Change range and directory as needed
