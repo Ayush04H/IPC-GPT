7775ef1b3f088bdad0d9217269e4fbe80f592ea7
Ayush04H <ayush050419@gmail.com>
2025-02-06 00:35:21 +0530

    fine tuned Model Evaluation Dataset added

--- frontend.py
+++ frontend.py
@@ -22,7 +22,7 @@ model.eval()
 
 # Prepare the prompt
 messages = [
-    {"role": "user", "content": "The What is the punishment for Harbouring such an officer, soldier, sailor or airman who has deserted comes under which section and what are the punishment??"}
+    {"role": "user", "content": "What does IPC section 128 state?"}
 ]
 
 # Tokenize input
